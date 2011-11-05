b83c458f022a09d2683b606149428a1493bcbdf4
Brian Norton <brian.nort@gmail.com>
2011-11-05 12:34:05 -0700

    updating a bad index specification

--- config/initializers/mongo_indexes.rb
+++ config/initializers/mongo_indexes.rb
@@ -4,8 +4,8 @@ MONGO_INDEX_DESC = -1
 Comment.ensure_index([[:rid, MONGO_INDEX_ASC]])
 Comment.ensure_index([[:lid, MONGO_INDEX_ASC]])
 
-Ranking.ensure_index({:nid => MONGO_INDEX_ASC, :uid => MONGO_INDEX_ASC})
-Ranking.ensure_index({:uid => MONGO_INDEX_ASC, :v => MONGO_INDEX_ASC})
+Ranking.ensure_index([[:nid, MONGO_INDEX_ASC], [:uid, MONGO_INDEX_ASC]])
+Ranking.ensure_index([[:uid, MONGO_INDEX_ASC], [:v, MONGO_INDEX_ASC]])
 
 RankingAverage.ensure_index([[:nid, MONGO_INDEX_ASC]])
 
