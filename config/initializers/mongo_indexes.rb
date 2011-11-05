2bbfaa5e27ffd2b278c92f12609089e275ee91e6
Brian Norton <brian.nort@gmail.com>
2011-11-05 12:26:00 -0700

    adding the right ensure indexes for rankings

--- config/initializers/mongo_indexes.rb
+++ config/initializers/mongo_indexes.rb
@@ -4,7 +4,8 @@ MONGO_INDEX_DESC = -1
 Comment.ensure_index([[:rid, MONGO_INDEX_ASC]])
 Comment.ensure_index([[:lid, MONGO_INDEX_ASC]])
 
-# Rating.ensure_index([[]])
+Ranking.ensure_index({:nid => MONGO_INDEX_ASC, :uid => MONGO_INDEX_ASC})
+Ranking.ensure_index({:uid => MONGO_INDEX_ASC, :v => MONGO_INDEX_ASC})
 
 RankingAverage.ensure_index([[:nid, MONGO_INDEX_ASC]])
 
@@ -12,6 +13,5 @@ Recommend.ensure_index([[:uid, MONGO_INDEX_ASC]])
 Recommend.ensure_index([[:to_uid, MONGO_INDEX_ASC]])
 Recommend.ensure_index([[:lid, MONGO_INDEX_ASC]])
 
-
 Detail.ensure_index([[:r, MONGO_INDEX_ASC]])
-# Like.ensure_index([[]])
+
