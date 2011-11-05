ac67d254375888e27e55f507c522fa74b5bd3c3f
Brian Norton <brian.nort@gmail.com>
2011-11-05 12:20:09 -0700

    updates for deploy and unicorn

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,41 +1,206 @@
-set :application, "justnom.it"
-set :repository,  "git@github.com:bnorton/nom_it.git"
+# "/tmp/unicorn.nom_it.pid"
 
-set :scm, :git
+# set :application, "justnom.it"
 
-set :user, :root
 
-role :web, "justnom.it"                          # Your HTTP server, Apache/etc
-role :app, "justnom.it"                          # This may be the same as your `Web` server
-role :db,  "justnom.it", :primary => true        # This is where Rails migrations will run
+# set :scm, :git
 
-set :rails_env, :production
-set :unicorn_binary, "unicorn"
-set :unicorn_config, "#{current_path}/config/unicorn.rb"
-set :unicorn_pid, "#{current_path}/tmp/pids/unicorn.pid"
+# set :user, :root
+
+role :web, "96.126.97.77"                          # Your HTTP server, Apache/etc
+role :app, "96.126.97.77"                          # This may be the same as your `Web` server
+role :db,  "96.126.97.77", :primary => true        # This is where Rails migrations will run
+
+# set :rails_env, :production
+# set :unicorn_binary, "unicorn"
+
+
+# config/deploy.rb 
+require "bundler/capistrano"
+
+set :scm,             :git
+set :repository,      "git@github.com:bnorton/nom_it.git"
+set :branch,          "origin/master"
+set :migrate_target,  :current
+set :ssh_options,     { :forward_agent => true }
+set :rails_env,       "production"
+set :deploy_to,       "/u/apps/justnom.it"
+set :normalize_asset_timestamps, false
+
+set :user,            "deployer"
+set :group,           "staff"
+set :use_sudo,        false
+
+# role :web,    "justnom.it"
+# role :app,    "justnom.it"
+# role :db,     "justnom.it", :primary => true
+
+set(:latest_release)  { fetch(:current_path) }
+set(:release_path)    { fetch(:current_path) }
+set(:current_release) { fetch(:current_path) }
+
+set(:current_revision)  { capture("cd #{current_path}; git rev-parse --short HEAD").strip }
+set(:latest_revision)   { capture("cd #{current_path}; git rev-parse --short HEAD").strip }
+set(:previous_revision) { capture("cd #{current_path}; git rev-parse --short HEAD@{1}").strip }
+
+default_environment["RAILS_ENV"] = 'production'
+
+# Use our ruby-1.9.2-p290@my_site gemset
+default_environment["PATH"]         = "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/rvm/bin"
+default_environment["GEM_HOME"]     = "/usr/local/rvm/gems"
+default_environment["GEM_PATH"]     = "/usr/local/rvm/gems/ree-1.8.7-2011.03@nom"
+default_environment["RUBY_VERSION"] = "ruby-1.8.7-p320"
+
+default_run_options[:shell] = 'bash'
 
 namespace :deploy do
-  task :start, :roles => :app, :except => { :no_release => true } do 
-    # run "cd #{current_path} && #{try_sudo} #{unicorn_binary} -c #{unicorn_config} -E #{rails_env} -D"
-    run "cd #{current_path} && #{unicorn_binary} -c #{unicorn_config} -E #{rails_env}"
+  desc "Deploy your application"
+  task :default do
+    update
+    restart
   end
-  task :stop, :roles => :app, :except => { :no_release => true } do 
-    # run "#{try_sudo} kill `cat #{unicorn_pid}`"
-    run "kill `cat #{unicorn_pid}`"
+
+  desc "Setup your git-based deployment app"
+  task :setup, :except => { :no_release => true } do
+    dirs = [deploy_to, shared_path]
+    dirs += shared_children.map { |d| File.join(shared_path, d) }
+    run "#{try_sudo} mkdir -p #{dirs.join(' ')} && #{try_sudo} chmod g+w #{dirs.join(' ')}"
+    run "git clone #{repository} #{current_path}"
   end
-  task :graceful_stop, :roles => :app, :except => { :no_release => true } do
-    # run "#{try_sudo} kill -s QUIT `cat #{unicorn_pid}`"
-    run "kill -s QUIT `cat #{unicorn_pid}`"
+
+  task :cold do
+    update
+    migrate
   end
-  task :reload, :roles => :app, :except => { :no_release => true } do
-    # run "#{try_sudo} kill -s USR2 `cat #{unicorn_pid}`"
-    run "kill -s USR2 `cat #{unicorn_pid}`"
+
+  task :update do
+    transaction do
+      update_code
+    end
   end
-  task :restart, :roles => :app, :except => { :no_release => true } do
-    stop
-    start
+
+  desc "Update the deployed code."
+  task :update_code, :except => { :no_release => true } do
+    run "cd #{current_path}; git fetch origin; git reset --hard #{branch}"
+    finalize_update
+  end
+
+  desc "Update the database (overwritten to avoid symlink)"
+  task :migrations do
+    transaction do
+      update_code
+    end
+    migrate
+    restart
+  end
+
+  task :finalize_update, :except => { :no_release => true } do
+    run "chmod -R g+w #{latest_release}" if fetch(:group_writable, true)
+
+    # mkdir -p is making sure that the directories are there for some SCM's that don't
+    # save empty folders
+    run <<-CMD
+      rm -rf #{latest_release}/log #{latest_release}/public/system #{latest_release}/tmp/pids &&
+      mkdir -p #{latest_release}/public &&
+      mkdir -p #{latest_release}/tmp &&
+      ln -s #{shared_path}/log #{latest_release}/log &&
+      ln -s #{shared_path}/system #{latest_release}/public/system &&
+      ln -s #{shared_path}/pids #{latest_release}/tmp/pids &&
+      ln -sf #{shared_path}/database.yml #{latest_release}/config/database.yml
+    CMD
+
+    if fetch(:normalize_asset_timestamps, true)
+      stamp = Time.now.utc.strftime("%Y%m%d%H%M.%S")
+      asset_paths = fetch(:public_children, %w(images stylesheets javascripts)).map { |p| "#{latest_release}/public/#{p}" }.join(" ")
+      run "find #{asset_paths} -exec touch -t #{stamp} {} ';'; true", :env => { "TZ" => "UTC" }
+    end
+  end
+
+  desc "Zero-downtime restart of Unicorn"
+  task :restart, :except => { :no_release => true } do
+    run "kill -s USR2 `cat /tmp/unicorn.my_site.pid`"
+  end
+
+  desc "Start unicorn"
+  task :start, :except => { :no_release => true } do
+    run "cd #{current_path} ; bundle exec unicorn_rails -c config/unicorn.rb -D"
+  end
+
+  desc "Stop unicorn"
+  task :stop, :except => { :no_release => true } do
+    run "kill -s QUIT `cat /tmp/unicorn.my_site.pid`"
+  end  
+
+  namespace :rollback do
+    desc "Moves the repo back to the previous version of HEAD"
+    task :repo, :except => { :no_release => true } do
+      set :branch, "HEAD@{1}"
+      deploy.default
+    end
+
+    desc "Rewrite reflog so HEAD@{1} will continue to point to at the next previous release."
+    task :cleanup, :except => { :no_release => true } do
+      run "cd #{current_path}; git reflog delete --rewrite HEAD@{1}; git reflog delete --rewrite HEAD@{1}"
+    end
+
+    desc "Rolls back to the previously deployed version."
+    task :default do
+      rollback.repo
+      rollback.cleanup
+    end
   end
 end
+
+def run_rake(cmd)
+  run "cd #{current_path}; #{rake} #{cmd}"
+end
+
+
+
+
+
+# set :application, "justnom.it"
+# set :repository,  "git@github.com:bnorton/nom_it.git"
+# 
+# set :scm, :git
+# 
+# set :user, :root
+# 
+# role :web, "justnom.it"                          # Your HTTP server, Apache/etc
+# role :app, "justnom.it"                          # This may be the same as your `Web` server
+# role :db,  "justnom.it", :primary => true        # This is where Rails migrations will run
+# 
+# set :rails_env, :production
+# set :unicorn_binary, "unicorn"
+# set :unicorn_config, "#{current_path}/config/unicorn.rb"
+# set :unicorn_pid, "#{current_path}/tmp/pids/unicorn.pid"
+# 
+# $:.unshift(File.expand_path('./lib', ENV['rvm_path'])) # Add RVM's lib directory to the load path.
+# require "rvm/capistrano"                  # Load RVM's capistrano plugin.
+# set :rvm_ruby_string, 'ree@nom'        # Or whatever env you want it to run in.
+# 
+# namespace :deploy do
+#   task :start, :roles => :app, :except => { :no_release => true } do 
+#     # run "cd #{current_path} && #{try_sudo} #{unicorn_binary} -c #{unicorn_config} -E #{rails_env} -D"
+#     run "cd #{current_path} && #{unicorn_binary} -c #{unicorn_config} -E #{rails_env}"
+#   end
+#   task :stop, :roles => :app, :except => { :no_release => true } do 
+#     # run "#{try_sudo} kill `cat #{unicorn_pid}`"
+#     run "kill `cat #{unicorn_pid}`"
+#   end
+#   task :graceful_stop, :roles => :app, :except => { :no_release => true } do
+#     # run "#{try_sudo} kill -s QUIT `cat #{unicorn_pid}`"
+#     run "kill -s QUIT `cat #{unicorn_pid}`"
+#   end
+#   task :reload, :roles => :app, :except => { :no_release => true } do
+#     # run "#{try_sudo} kill -s USR2 `cat #{unicorn_pid}`"
+#     run "kill -s USR2 `cat #{unicorn_pid}`"
+#   end
+#   task :restart, :roles => :app, :except => { :no_release => true } do
+#     stop
+#     start
+#   end
+# end
 
 # load 'deploy' if respond_to? :namespace
 # 
--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,146 +1,212 @@
-# # Sample verbose configuration file for Unicorn (not Rack)
-# #
-# # This configuration file documents many features of Unicorn
-# # that may not be needed for some applications. See
-# # http://unicorn.bogomips.org/examples/unicorn.conf.minimal.rb
-# # for a much simpler configuration file.
-# #
-# # See http://unicorn.bogomips.org/Unicorn/Configurator.html for complete
-# # documentation.
-# 
-# # Use at least one worker per core if you're on a dedicated server,
-# # more will usually help for _short_ waits on databases/caches.
-# worker_processes 4
-# 
-# # Since Unicorn is never exposed to outside clients, it does not need to
-# # run on the standard HTTP port (80), there is no reason to start Unicorn
-# # as root unless it's from system init scripts.
-# # If running the master process as root and the workers as an unprivileged
-# # user, do this to switch euid/egid in the workers (also chowns logs):
-# # user "unprivileged_user", "unprivileged_group"
-# 
-# # Help ensure your application will always spawn in the symlinked
-# # "current" directory that Capistrano sets up.
-# APP_PATH = "/u/apps/justnom.it/current"
-# working_directory APP_PATH
-# 
-# # listen on both a Unix domain socket and a TCP port,
-# # we use a shorter backlog for quicker failover when busy
-# listen "/tmp/sockets/unicorn.sock", :backlog => 64
-# listen 8080, :tcp_nopush => true
-# 
-# # nuke workers after 30 seconds instead of 60 seconds (the default)
-# timeout 30
-# 
-# # feel free to point this anywhere accessible on the filesystem
-# pid APP_PATH + "/tmp/pids/unicorn.pid"
-# 
-# # By default, the Unicorn logger will write to stderr.
-# # Additionally, ome applications/frameworks log to stderr or stdout,
-# # so prevent them from going to /dev/null when daemonized here:
-# stderr_path APP_PATH + "/log/unicorn.stderr.log"
-# stdout_path APP_PATH + "/log/unicorn.stderr.log"
-# 
-# # combine REE with "preload_app true" for memory savings
-# # http://rubyenterpriseedition.com/faq.html#adapt_apps_for_cow
-# preload_app true
-# GC.respond_to?(:copy_on_write_friendly=) and
-#   GC.copy_on_write_friendly = true
-# 
-# before_fork do |server, worker|
-#   # the following is highly recomended for Rails + "preload_app true"
-#   # as there's no need for the master process to hold a connection
-#   defined?(ActiveRecord::Base) and
-#     ActiveRecord::Base.connection.disconnect!
-# 
-#   # The following is only recommended for memory/DB-constrained
-#   # installations.  It is not needed if your system can house
-#   # twice as many worker_processes as you have configured.
-#   #
-#   # # This allows a new master process to incrementally
-#   # # phase out the old master process with SIGTTOU to avoid a
-#   # # thundering herd (especially in the "preload_app false" case)
-#   # # when doing a transparent upgrade.  The last worker spawned
-#   # # will then kill off the old master process with a SIGQUIT.
-#   # old_pid = "#{server.config[:pid]}.oldbin"
-#   # if old_pid != server.pid
-#   #   begin
-#   #     sig = (worker.nr + 1) >= server.worker_processes ? :QUIT : :TTOU
-#   #     Process.kill(sig, File.read(old_pid).to_i)
-#   #   rescue Errno::ENOENT, Errno::ESRCH
-#   #   end
-#   # end
-#   #
-#   # Throttle the master from forking too quickly by sleeping.  Due
-#   # to the implementation of standard Unix signal handlers, this
-#   # helps (but does not completely) prevent identical, repeated signals
-#   # from being lost when the receiving process is busy.
-#   # sleep 1
-# end
-# 
-# after_fork do |server, worker|
-#   # per-process listener ports for debugging/admin/migrations
-#   # addr = "127.0.0.1:#{9293 + worker.nr}"
-#   # server.listen(addr, :tries => -1, :delay => 5, :tcp_nopush => true)
-# 
-#   # the following is *required* for Rails + "preload_app true",
-#   defined?(ActiveRecord::Base) and
-#     ActiveRecord::Base.establish_connection
-# 
-#   # if preload_app is true, then you may also want to check and
-#   # restart any other shared sockets/descriptors such as Memcached,
-#   # and Redis.  TokyoCabinet file handles are safe to reuse
-#   # between any number of forked children (assuming your kernel
-#   # correctly implements pread()/pwrite() system calls)
-# end
+# config/unicorn.rb
+# Set environment to development unless something else is specified
+env = ENV["RAILS_ENV"] || "development"
 
-
-ENV['MY_RUBY_HOME'] = run "`which ruby`"
-APP_ROOT = File.expand_path(File.dirname(File.dirname(__FILE__)))
-
-if ENV['MY_RUBY_HOME'] && ENV['MY_RUBY_HOME'].include?('rvm')
-  begin
-    rvm_path = File.dirname(File.dirname(ENV['MY_RUBY_HOME']))
-    rvm_lib_path = File.join(rvm_path, 'lib')
-    $LOAD_PATH.unshift rvm_lib_path
-    require 'rvm'
-    RVM.use_from_path! APP_ROOT
-  rescue LoadError
-    raise "RVM ruby lib is currently unavailable."
-  end
-end
-
-ENV['BUNDLE_GEMFILE'] = File.expand_path('../Gemfile', File.dirname(__FILE__))
-require 'bundler/setup'
-
+# See http://unicorn.bogomips.org/Unicorn/Configurator.html for complete
+# documentation.
 worker_processes 4
-working_directory APP_ROOT
 
+# listen on both a Unix domain socket and a TCP port,
+# we use a shorter backlog for quicker failover when busy
+listen "/tmp/nom_it.socket", :backlog => 64
+
+# Preload our app for more speed
 preload_app true
 
+# nuke workers after 30 seconds instead of 60 seconds (the default)
 timeout 30
 
-listen APP_ROOT + "/tmp/sockets/unicorn.sock", :backlog => 64
+pid "/tmp/unicorn.nom_it.pid"
 
-pid APP_ROOT + "/tmp/pids/unicorn.pid"
+# Production specific settings
+if env == "production"
+  # Help ensure your application will always spawn in the symlinked
+  # "current" directory that Capistrano sets up.
+  working_directory "/u/apps/justnom.it/current"
 
-stderr_path APP_ROOT + "/log/unicorn.stderr.log"
-stdout_path APP_ROOT + "/log/unicorn.stdout.log"
+  # feel free to point this anywhere accessible on the filesystem
+  user 'deployer', 'staff'
+  shared_path = "#{working_directory}/tmp"
+
+  stderr_path "#{shared_path}/log/unicorn.stderr.log"
+  stdout_path "#{shared_path}/log/unicorn.stdout.log"
+end
 
 before_fork do |server, worker|
-  defined?(ActiveRecord::Base) && ActiveRecord::Base.connection.disconnect!
+  # the following is highly recomended for Rails + "preload_app true"
+  # as there's no need for the master process to hold a connection
+  if defined?(ActiveRecord::Base)
+    ActiveRecord::Base.connection.disconnect!
+  end
 
-  old_pid = RAILS_ROOT + '/tmp/pids/unicorn.pid.oldbin'
+  # Before forking, kill the master process that belongs to the .oldbin PID.
+  # This enables 0 downtime deploys.
+  old_pid = "/tmp/unicorn.my_site.pid.oldbin"
   if File.exists?(old_pid) && server.pid != old_pid
     begin
       Process.kill("QUIT", File.read(old_pid).to_i)
     rescue Errno::ENOENT, Errno::ESRCH
-      puts "Old master alerady dead"
+      # someone else did our job for us
     end
   end
 end
 
 after_fork do |server, worker|
-  defined?(ActiveRecord::Base) && ActiveRecord::Base.establish_connection
+  # the following is *required* for Rails + "preload_app true",
+  if defined?(ActiveRecord::Base)
+    ActiveRecord::Base.establish_connection
+  end
+
+  # if preload_app is true, then you may also want to check and
+  # restart any other shared sockets/descriptors such as Memcached,
+  # and Redis.  TokyoCabinet file handles are safe to reuse
+  # between any number of forked children (assuming your kernel
+  # correctly implements pread()/pwrite() system calls)
 end
 
+# # # Sample verbose configuration file for Unicorn (not Rack)
+# # #
+# # # This configuration file documents many features of Unicorn
+# # # that may not be needed for some applications. See
+# # # http://unicorn.bogomips.org/examples/unicorn.conf.minimal.rb
+# # # for a much simpler configuration file.
+# # #
+# # # See http://unicorn.bogomips.org/Unicorn/Configurator.html for complete
+# # # documentation.
+# # 
+# # # Use at least one worker per core if you're on a dedicated server,
+# # # more will usually help for _short_ waits on databases/caches.
+# # worker_processes 4
+# # 
+# # # Since Unicorn is never exposed to outside clients, it does not need to
+# # # run on the standard HTTP port (80), there is no reason to start Unicorn
+# # # as root unless it's from system init scripts.
+# # # If running the master process as root and the workers as an unprivileged
+# # # user, do this to switch euid/egid in the workers (also chowns logs):
+# # # user "unprivileged_user", "unprivileged_group"
+# # 
+# # # Help ensure your application will always spawn in the symlinked
+# # # "current" directory that Capistrano sets up.
+# # APP_PATH = "/u/apps/justnom.it/current"
+# # working_directory APP_PATH
+# # 
+# # # listen on both a Unix domain socket and a TCP port,
+# # # we use a shorter backlog for quicker failover when busy
+# # listen "/tmp/sockets/unicorn.sock", :backlog => 64
+# # listen 8080, :tcp_nopush => true
+# # 
+# # # nuke workers after 30 seconds instead of 60 seconds (the default)
+# # timeout 30
+# # 
+# # # feel free to point this anywhere accessible on the filesystem
+# # pid APP_PATH + "/tmp/pids/unicorn.pid"
+# # 
+# # # By default, the Unicorn logger will write to stderr.
+# # # Additionally, ome applications/frameworks log to stderr or stdout,
+# # # so prevent them from going to /dev/null when daemonized here:
+# # stderr_path APP_PATH + "/log/unicorn.stderr.log"
+# # stdout_path APP_PATH + "/log/unicorn.stderr.log"
+# # 
+# # # combine REE with "preload_app true" for memory savings
+# # # http://rubyenterpriseedition.com/faq.html#adapt_apps_for_cow
+# # preload_app true
+# # GC.respond_to?(:copy_on_write_friendly=) and
+# #   GC.copy_on_write_friendly = true
+# # 
+# # before_fork do |server, worker|
+# #   # the following is highly recomended for Rails + "preload_app true"
+# #   # as there's no need for the master process to hold a connection
+# #   defined?(ActiveRecord::Base) and
+# #     ActiveRecord::Base.connection.disconnect!
+# # 
+# #   # The following is only recommended for memory/DB-constrained
+# #   # installations.  It is not needed if your system can house
+# #   # twice as many worker_processes as you have configured.
+# #   #
+# #   # # This allows a new master process to incrementally
+# #   # # phase out the old master process with SIGTTOU to avoid a
+# #   # # thundering herd (especially in the "preload_app false" case)
+# #   # # when doing a transparent upgrade.  The last worker spawned
+# #   # # will then kill off the old master process with a SIGQUIT.
+# #   # old_pid = "#{server.config[:pid]}.oldbin"
+# #   # if old_pid != server.pid
+# #   #   begin
+# #   #     sig = (worker.nr + 1) >= server.worker_processes ? :QUIT : :TTOU
+# #   #     Process.kill(sig, File.read(old_pid).to_i)
+# #   #   rescue Errno::ENOENT, Errno::ESRCH
+# #   #   end
+# #   # end
+# #   #
+# #   # Throttle the master from forking too quickly by sleeping.  Due
+# #   # to the implementation of standard Unix signal handlers, this
+# #   # helps (but does not completely) prevent identical, repeated signals
+# #   # from being lost when the receiving process is busy.
+# #   # sleep 1
+# # end
+# # 
+# # after_fork do |server, worker|
+# #   # per-process listener ports for debugging/admin/migrations
+# #   # addr = "127.0.0.1:#{9293 + worker.nr}"
+# #   # server.listen(addr, :tries => -1, :delay => 5, :tcp_nopush => true)
+# # 
+# #   # the following is *required* for Rails + "preload_app true",
+# #   defined?(ActiveRecord::Base) and
+# #     ActiveRecord::Base.establish_connection
+# # 
+# #   # if preload_app is true, then you may also want to check and
+# #   # restart any other shared sockets/descriptors such as Memcached,
+# #   # and Redis.  TokyoCabinet file handles are safe to reuse
+# #   # between any number of forked children (assuming your kernel
+# #   # correctly implements pread()/pwrite() system calls)
+# # end
+# 
+# 
+# ENV['MY_RUBY_HOME'] = run "`which ruby`"
+# APP_ROOT = File.expand_path(File.dirname(File.dirname(__FILE__)))
+# 
+# if ENV['MY_RUBY_HOME'] && ENV['MY_RUBY_HOME'].include?('rvm')
+#   begin
+#     rvm_path = File.dirname(File.dirname(ENV['MY_RUBY_HOME']))
+#     rvm_lib_path = File.join(rvm_path, 'lib')
+#     $LOAD_PATH.unshift rvm_lib_path
+#     require 'rvm'
+#     RVM.use_from_path! APP_ROOT
+#   rescue LoadError
+#     raise "RVM ruby lib is currently unavailable."
+#   end
+# end
+# 
+# ENV['BUNDLE_GEMFILE'] = File.expand_path('../Gemfile', File.dirname(__FILE__))
+# require 'bundler/setup'
+# 
+# worker_processes 4
+# working_directory APP_ROOT
+# 
+# preload_app true
+# 
+# timeout 30
+# 
+# listen APP_ROOT + "/tmp/sockets/unicorn.sock", :backlog => 64
+# 
+# pid APP_ROOT + "/tmp/pids/unicorn.pid"
+# 
+# stderr_path APP_ROOT + "/log/unicorn.stderr.log"
+# stdout_path APP_ROOT + "/log/unicorn.stdout.log"
+# 
+# before_fork do |server, worker|
+#   defined?(ActiveRecord::Base) && ActiveRecord::Base.connection.disconnect!
+# 
+#   old_pid = RAILS_ROOT + '/tmp/pids/unicorn.pid.oldbin'
+#   if File.exists?(old_pid) && server.pid != old_pid
+#     begin
+#       Process.kill("QUIT", File.read(old_pid).to_i)
+#     rescue Errno::ENOENT, Errno::ESRCH
+#       puts "Old master alerady dead"
+#     end
+#   end
+# end
+# 
+# after_fork do |server, worker|
+#   defined?(ActiveRecord::Base) && ActiveRecord::Base.establish_connection
+# end
+# 
