bc2f881b03ef98a4afd2d9885ed66b9a43643d58
Brian Norton <brian.nort@gmail.com>
2011-11-18 08:42:01 -0800

    updates

--- config/server.production1.bash
+++ config/server.production1.bash
@@ -20,10 +20,6 @@ make && make install
 cd /usr/local/mysql
 scripts/mysql_install_db
 
-# cp support-files/my-medium.cnf /etc/my.cnf
-# bin/mysqld_safe --user=mysql &
-
-# Next command is optional
 cp support-files/mysql.server /etc/init.d/mysql.server
 
 sudo apt-get install -y mysql-client libmysqlclient16-dev libmysqlclient16 libmysql-ruby libmysqlclient-dev
@@ -32,6 +28,78 @@ git config --global user.name "Nom Production"
 git config --global user.email "deployer@justnom.it"
 git config --global github.user bnorton
 git config --global github.token a97228eb813ef5b0734ebc3dee4ce5e5
+
+cd ~/; cat > .ssh/id_rsa <<ID_RSA
+-----BEGIN RSA PRIVATE KEY-----
+MIIEpAIBAAKCAQEA8U534rcKwr/HzPp7rFHDCNlsoeik3en2TROkdyrIwpCpCU4B
+vFps+f/J/0Rkorjimw4XmvSc5xju8Jd0ZJqp4p+pOlHyvTvmxvEYeiICcaLkeVFC
+YavidCEc+4NdHP/3S7zdIMPksGQ/NkjNiW01xDlqXwoCjaZUK7XBgXviQ0N5/Ahx
+L1oQHGmIM8LQY7lAemreTaakKCSSc+gWnawz/mCsEChsHBUblhgQk++iII+JU2k7
++/oSkPNnR3FuHdJQAH3AUnno0eGmnkglzT0pimzRNwpMB/NqL3Fn49NYfwT1uCm0
+DLB3xLOIeZofVnc/c2QZ3u2ESmSIw5j/EKKXTwIDAQABAoIBADSmFTwvCbcCFvrf
+L/qiADa/EZel5crRUD7y6TBi8liZLXhtP62z4REOwSjj6D0kr7R696WEd3NomkF+
+GhQVNrnOciLaXGbzWd/QHLIRF41pqAXcN+qNkSQbUXKC797y7iblNy3977iYtr4G
+VmoEnPWKPW4tBe+X+cdrqaOxvuVDuDOLqbbT+D0538rpomrUsryYuCpVoFS0nSVy
+9fCl+p8ubz2dENttH6/PhIr8ZEDyiWoSX6JJpfer6AsAWtpvAWfkPlo/M1dBxha2
+nNIEVeLRA5GNgcOhiva1WrOixTcfERkSPrpUeTUQiQIqRRABtzs6AMAi1OOgdOgf
+EUw0sckCgYEA+5oa2R52xMV9e9vb3IOUezzGF21imLd75Po2OA6OmYQqx69qR221
+sh5ENAGgMRy3yieEjvhvqkd+OzR1E1sFwhsPp/w+TWPMxlg3LZQa+R4sV4gxEk2g
+wkl4nQ60bLzVb+yofFEP4QnzVINlzj6Fks5NbcEKPqkVK033mfSILR0CgYEA9YZK
+z+XRbPDMoKQAbFbAUpCHK0JYFaOYTHlRIW9f2+AdiTaTKrSszl9eMchO2KtZqjax
+WinG87HRjv3oCW4HeiH8R3MeczFnsNbIzz/BR7HKTmsZ1UBzA7PD4W08smXstBKX
+wvmRvL8fsrkKojoAC+jdz8uu3lHQHc2AJBR1ZlsCgYEAjYA7/UQO09emHRSncDto
+NG8XkXFpdC4tNbgq6hf1xwz36loTUZy4BTbUcNNBPp3CF6Vl/epnEsMmkTGNbpdQ
+g7wob/eDKo8oSZyvW6jiCp0XnxrvTjXuZZZgiSQOAOGqwEm+8Du+zHeGLE/B3951
+zPzNux5IHrmFOFefGyzVsVkCgYAmj6nuTwT+XC93R2q9mT5peUOResEE3QXNdPxW
+CP0ANonNBCJHActmOjo8DV68zuStRBvEsm0J1zK3h49K89n+x5msyxrMMsU7U/CE
+BFph3T2N5WpQQOgPe8tW+2YeCr4LZiQpvjKydz4OPtu5sOxS8obr3OyhBC5wj9cB
+U6lAowKBgQC+UnMjZXT1/lovR5uqTGQcnPHnHQq79mqj5Kf212Wp1iF2vBv3LZCw
+L7PaaevRcwsmJfyW3Ex2YAY4Uc/eb/c2IGza6mHZxVQk2Vus+Cu+r13THyueyWcA
+Upiibnn0anHDNv5sbELbrf1fNV6T6OkL1NJaKDR7hHWnOsNurJxgzw==
+-----END RSA PRIVATE KEY-----
+ID_RSA
+
+cat > .ssh/id_rsa.pub <<ID_RSA_PUB
+ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDxTnfitwrCv8fM+nusUcMI2Wyh6KTd6fZNE6R3KsjCkKkJTgG8Wmz5/8n/RGSiuOKbDhea9JznGO7wl3Rkmqnin6k6UfK9O+bG8Rh6IgJxouR5UUJhq+J0IRz7g10c//dLvN0gw+SwZD82SM2JbTXEOWpfCgKNplQrtcGBe+JDQ3n8CHEvWhAcaYgzwtBjuUB6at5NpqQoJJJz6BadrDP+YKwQKGwcFRuWGBCT76Igj4lTaTv7+hKQ82dHcW4d0lAAfcBSeejR4aaeSCXNPSmKbNE3CkwH82ovcWfj01h/BPW4KbQMsHfEs4h5mh9Wdz9zZBne7YRKZIjDmP8QopdP brian.nort@gmail.com
+ID_RSA_PUB
+
+cat > .ssh/authorized_keys <<AUTH
+ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCQxUAAvFjYWQ3BRlvuAkamL/aeSxraa8w6IuSakjBYnHcg4PmSxDF2Hxlnaq26LUUxm3aLOZdQsgsppmZuWcCopHcwsujLLlfDzUOzWmvvtyKG4Ubq45Uc8jHHGaGF1ObZOxALAvHKenBrzU9DG29FmJX0TwrIZySKOqKdTbExvR2R4kt204cN18sTsOi8CjBd3Ba+PA0W0TQS5sDi7KAuaY35HzXt+j3Ljrho2p6Qus+QHtuIVnLnQTNBab3/dQ0guosjEgU+lLmwJbT4GonvM9S2VQ25d0xqvXARgUMn4QMAwYaiDqtBumH65AaDfgvad5v6J73StCkx6FUEBTST bnorton_10-4_32bit_server
+ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDxTnfitwrCv8fM+nusUcMI2Wyh6KTd6fZNE6R3KsjCkKkJTgG8Wmz5/8n/RGSiuOKbDhea9JznGO7wl3Rkmqnin6k6UfK9O+bG8Rh6IgJxouR5UUJhq+J0IRz7g10c//dLvN0gw+SwZD82SM2JbTXEOWpfCgKNplQrtcGBe+JDQ3n8CHEvWhAcaYgzwtBjuUB6at5NpqQoJJJz6BadrDP+YKwQKGwcFRuWGBCT76Igj4lTaTv7+hKQ82dHcW4d0lAAfcBSeejR4aaeSCXNPSmKbNE3CkwH82ovcWfj01h/BPW4KbQMsHfEs4h5mh9Wdz9zZBne7YRKZIjDmP8QopdP brian.nort@gmail.com
+AUTH
+
+cat > .ssh/known_hosts <<KNOWN
+github.com,207.97.227.239 ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAq2A7hRGmdnm9tUDbO9IDSwBK6TbQa+PXYPCPy6rbTrTtw7PHkccKrpp0yVhp5HdEIcKr6pLlVDBfOLX9QUsyCOV0wzfjIJNlGEYsdlLJizHhbn2mUjvSAHQqZETYP81eFzLQNnPHt4EVVUh7VfDESU84KezmD5QlWpXLmvU31/yMf+Se8xhHTvKSCZIFImWwoG6mbUoWf9nzpIoaSjB+weqqUUmpaaasXVal72J+UX2B+2RPW3RcT0eOzQgqlJL3RKrTJvdsjE3JEAvGq3lGHSZXy28G3skua2SmVi/w4yCE6gbODqnTWlg7+wC604ydGXA8VJiS5ap43JXiUFFAaQ==
+|1|EkdpYNUylA9W9SipZJrNw8m3kps=|lw6SOxc2Hgw5Wg0IKK9tUDMsWqk= ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAq2A7hRGmdnm9tUDbO9IDSwBK6TbQa+PXYPCPy6rbTrTtw7PHkccKrpp0yVhp5HdEIcKr6pLlVDBfOLX9QUsyCOV0wzfjIJNlGEYsdlLJizHhbn2mUjvSAHQqZETYP81eFzLQNnPHt4EVVUh7VfDESU84KezmD5QlWpXLmvU31/yMf+Se8xhHTvKSCZIFImWwoG6mbUoWf9nzpIoaSjB+weqqUUmpaaasXVal72J+UX2B+2RPW3RcT0eOzQgqlJL3RKrTJvdsjE3JEAvGq3lGHSZXy28G3skua2SmVi/w4yCE6gbODqnTWlg7+wC604ydGXA8VJiS5ap43JXiUFFAaQ==
+|1|vN+zFVFo3PuK1okybPzEuiA3bFg=|veNWwSfP5t6xJTBdv/H5pbOUvKU= ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAq2A7hRGmdnm9tUDbO9IDSwBK6TbQa+PXYPCPy6rbTrTtw7PHkccKrpp0yVhp5HdEIcKr6pLlVDBfOLX9QUsyCOV0wzfjIJNlGEYsdlLJizHhbn2mUjvSAHQqZETYP81eFzLQNnPHt4EVVUh7VfDESU84KezmD5QlWpXLmvU31/yMf+Se8xhHTvKSCZIFImWwoG6mbUoWf9nzpIoaSjB+weqqUUmpaaasXVal72J+UX2B+2RPW3RcT0eOzQgqlJL3RKrTJvdsjE3JEAvGq3lGHSZXy28G3skua2SmVi/w4yCE6gbODqnTWlg7+wC604ydGXA8VJiS5ap43JXiUFFAaQ==
+KNOWN
+
+cat > .ssh/environment <<ENV
+SSH_AUTH_SOCK=/tmp/ssh-ICqeeMAh3658/agent.3658; export SSH_AUTH_SOCK;
+SSH_AGENT_PID=3660; export SSH_AGENT_PID;
+ENV
+
+chmod 700 ~/.ssh
+chmod 600 .ssh/id_rsa
+chmod 600 .ssh/authorized_keys
+
+cd ~/; git clone git@github.com:bnorton/nom_it.git
+
+sudo mv /etc/nginx/nginx.conf /etc/nginx/nginx.conf.original
+sudo mkdir -p /var /var/log /var/log/nginx/
+
+sudo cd nom_it/config
+sudo cp nginx.conf.example /etc/nginx/nginx.conf
+sudo cp my.cnf.example /etc/my.cnf
+
+cd ~/src
+
+wget http://fastdl.mongodb.org/linux/mongodb-linux-x86_64-2.0.1.tgz
+tar -xzf mongodb-linux-x86_64-2.0.1.tgz
+sudo cp mongodb-linux-x86_64-2.0.1/bin/* /usr/bin/
+
+sudo mkdir /data && sudo mkdir -p /data/db && sudo mkdir -p /log && sudo mkdir -p /log/mongodb
+sudo touch /log/mongodb/production.log
 
 cat > ~/.gemrc <<GEMRC
 ---
@@ -53,192 +121,3 @@ rvm_trust_rvmrcs_flag=1
 RVMRC
 
 echo 'RAILS_ENV=production' >> /etc/environment
-
-cat > /etc/my.cnf <<MY_CNF
-#---------------------------------------------------------------------------
-# Example MySQL config file for large systems.
-#
-# This is for a large system with memory = 512M where the system runs mainly
-# MySQL.
-#
-# You can copy this file to
-# /etc/my.cnf to set global options,
-# mysql-data-dir/my.cnf to set server-specific options (in this
-# installation this directory is @localstatedir@) or
-# ~/.my.cnf to set user-specific options.
-#
-# In this file, you can use all long options that a program supports.
-# If you want to know which options a program supports, run the program
-# with the "--help" option.
-
-# The following options will be passed to all MySQL clients
-[client]
-#password	= your_password
-port  = 8008
-socket  = /tmp/mysql.sock
-
-# Here follows entries for some specific programs
-
-# The MySQL server
-[mysqld]
-port  = 8008
-socket  = /tmp/mysql.sock
-skip-locking
-
-# Caches and Buffer Sizes
-key_buffer = 256M
-max_allowed_packet=16M
-table_cache = 256
-sort_buffer_size = 2M
-read_buffer_size = 2M
-read_rnd_buffer_size = 4M
-record_buffer = 1M
-myisam_sort_buffer_size = 128M
-thread_cache = 128
-query_cache_limit = 2M
-query_cache_type = 1
-query_cache_size = 32M
-key_buffer = 16M
-join_buffer = 2M
-table_cache = 1024
-
-#Time Outs
-interactive_timeout = 100
-wait_timeout = 100
-connect_timeout = 10
-
-# Try number of CPU's*2 for thread_concurrency
-thread_concurrency = 2
-
-# Maximum connections allowed
-max_connections = 500
-max_user_connections = 50
-max_connect_errors = 10
-
-# Don't listen on a TCP/IP port at all. This can be a security enhancement,
-# if all processes that need to connect to mysqld run on the same host.
-# All interaction with mysqld must be made via Unix sockets or named pipes.
-# Note that using this option without enabling named pipes on Windows
-# (via the "enable-named-pipe" option) will render mysqld useless!
-# 
-#skip-networking
-
-# Replication Master Server (default)
-# binary logging is required for replication
-log-bin
-
-# required unique id between 1 and 2^32 - 1
-# defaults to 1 if master-host is not set
-# but will not function as a master if omitted
-server-id	= 1
-
-# Replication Slave (comment out master section to use this)
-#
-# To configure this host as a replication slave, you can choose between
-# two methods :
-#
-# 1) Use the CHANGE MASTER TO command (fully described in our manual) -
-#    the syntax is:
-#
-#    CHANGE MASTER TO MASTER_HOST=<host>, MASTER_PORT=<port>,
-#    MASTER_USER=<user>, MASTER_PASSWORD=<password> ;
-#
-#    where you replace <host>, <user>, <password> by quoted strings and
-#    <port> by the master's port number (3306 by default).
-#
-#    Example:
-#
-#    CHANGE MASTER TO MASTER_HOST='125.564.12.1', MASTER_PORT=3306,
-#    MASTER_USER='joe', MASTER_PASSWORD='secret';
-#
-# OR
-#
-# 2) Set the variables below. However, in case you choose this method, then
-#    start replication for the first time (even unsuccessfully, for example
-#    if you mistyped the password in master-password and the slave fails to
-#    connect), the slave will create a master.info file, and any later
-#    change in this file to the variables' values below will be ignored and
-#    overridden by the content of the master.info file, unless you shutdown
-#    the slave server, delete master.info and restart the slaver server.
-#    For that reason, you may want to leave the lines below untouched
-#    (commented) and instead use CHANGE MASTER TO (see above)
-#
-# required unique id between 2 and 2^32 - 1
-# (and different from the master)
-# defaults to 2 if master-host is set
-# but will not function as a slave if omitted
-#server-id      = 2
-#
-# The replication master for this slave - required
-#master-host    =  <hostname>
-#
-# The username the slave will use for authentication when connecting
-# to the master - required
-#master-user    =  <username>
-#
-# The password the slave will authenticate with when connecting to
-# the master - required
-#master-password =  <password>
-#
-# The port the master is listening on.
-# optional - defaults to 3306
-#master-port    =  <port>
-#
-# binary logging - not required for slaves, but recommended
-#log-bin
-
-# Point the following paths to different dedicated disks
-#tmpdir  = /tmp/  
-#log-update  = /path-to-dedicated-directory/hostname
-
-# Uncomment the following if you are using BDB tables
-#bdb_cache_size = 64M
-#bdb_max_lock = 100000
-
-# Uncomment the following if you are using InnoDB tables
-innodb_data_home_dir = /var/lib/mysql/
-innodb_data_file_path = ibdata1:10M:autoextend
-innodb_log_group_home_dir = /var/log/innodblogs/
-innodb_log_arch_dir = /var/log/innodblogsarchive/
-# You can set .._buffer_pool_size up to 50 - 80 %
-# of RAM but beware of setting memory usage too high
-innodb_buffer_pool_size = 160M
-innodb_additional_mem_pool_size = 20M
-# Set .._log_file_size to 25 % of buffer pool size
-innodb_log_file_size = 40M
-innodb_log_buffer_size = 8M
-innodb_flush_log_at_trx_commit = 1
-innodb_lock_wait_timeout = 50
-
-[mysqldump]
-quick
-max_allowed_packet = 16M
-
-[safe_mysqld]
-#err-log=/var/log/mysqld.log
-#pid-file=/var/lib/mysql/mysql.pid <-- Not necessary
-open_files_limit=8192
-
-
-[mysql]
-no-auto-rehash
-# Remove the next comment character if you are not familiar with SQL
-#safe-updates
-
-[isamchk]
-key_buffer = 128M
-sort_buffer_size = 128M
-read_buffer = 2M
-write_buffer = 2M
-
-[myisamchk]
-key_buffer = 128M
-sort_buffer_size = 128M
-read_buffer = 2M
-write_buffer = 2M
-
-[mysqlhotcopy]
-interactive-timeout/log
-#------------------------------------------------------------------------------------------
-MY_CNF
-
--- config/server.production3.bash
+++ config/server.production3.bash
@@ -18,36 +18,11 @@ rvm --default use ree@nom
 rvmsudo gem update --system
 rvmsudo gem install bundler rails rake rack unicorn mysql2 json bson bson_ext --no-ri --no-rdoc
 
-cd ~/src
-
-wget http://fastdl.mongodb.org/linux/mongodb-linux-x86_64-2.0.1.tgz
-tar -xzf mongodb-linux-x86_64-2.0.1.tgz
-sudo cp mongodb-linux-x86_64-2.0.1/bin/* /usr/bin/
-
-sudo mkdir /data && sudo mkdir -p /data/db && sudo mkdir -p /log && sudo mkdir -p /log/mongodb
-sudo touch /log/mongodb/production.log
-
-sudo /usr/bin/mongod --dbpath=/data/db --logpath=/log/mongodb/production.log --journal &
-
 cd ~/
-
-sudo mkdir -p /var /var/log /var/log/nginx/
-
-sudo mv /etc/nginx/nginx.conf /etc/nginx/nginx.conf.original
-
-sudo cat > /etc/nginx/nginx.conf <<NGINX_CONF
-NGINX_CONF
-
-sudo cat > /etc/nginx/sites-enabled/default <<DEFAULT
-DEFAULT
 
 sudo /etc/init.d/nginx stop
 sudo /etc/init.d/nginx start
 
-echo 'scp .ssh files into the right places'
-read -p 'done? [y|n]> '
-if [[ $REPLY =~ ^[Yy]$ ]]
-  then
-    echo 'checking git config'
-    ssh -T git@github.com
-fi
+sudo /usr/bin/mongod --dbpath=/data/db --logpath=/log/mongodb/production.log --journal &
+
+bin/mysqld_safe &
