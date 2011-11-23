737bf9b188d29f25164bdf39823e3096687582ea
Brian Norton <brian.nort@gmail.com>
2011-11-23 15:23:21 -0800

    overhaul the status class

--- app/helpers/api.rb
+++ app/helpers/api.rb
@@ -6,10 +6,10 @@ module NOM
       end
       # users
       def me
-        { :endpoint => "/users/me", :required => [:token], :optional => [:nid,:email], :example => "/users/me?token={some_token}", :description => {  } }
+        { :endpoint => "/users/me", :required => [:auth_token], :optional => [:user_nid,:email], :example => "/users/me?auth_token={your_auth_token}", :description => {  } }
       end
       def login
-        { :endpoint => "/users/login", :required => [:password], :optional => [:nid,:email], :example => "/users/login?nid={nid}&password={some_pass}", :description => {  } }
+        { :endpoint => "/users/login", :required => [:password], :optional => [:user_nid,:email], :example => "/users/login?user_nid={user_nid}&password={some_pass}", :description => {  } }
       end
       def register
         { :endpoint => "/users/register", :required => [:email,:password], :optional => [:name,:city,:regtype,:fb_hash,:tw_hash], :example => "/users/register?email={email@example.com}&password={some_pass}&name={real_name}", :description => {  } }
@@ -18,45 +18,45 @@ module NOM
         { :endpoint => "/users/search", :required => [:q], :optional => [:email,:screen_name], :example => "/users/search?q={nort}", :description => {  } }
       end
       def udetail
-        { :endpoint => "/users/detail", :required => [:nid], :optional => [:nids], :example => "/users/detail?nids={comma,separated,nids}", :description => {  } }
+        { :endpoint => "/users/detail", :required => [:user_nid], :optional => [:user_nids], :example => "/users/detail?user_nids={comma,separated,user_nids}", :description => {  } }
       end
       def ucheck
         { :endpoint => "/users/check", :required => [:screen_name], :optional => [:timeout], :example => "/users/check?screen_name={some_screen_name}", :description => {  } }
       end
       def urecommended
-        { :endpoint => "/users/{:nid}/recommended", :required => [:nid], :optional => [], :example => "/users/", :description => {  } }
+        { :endpoint => "/users/{:user_nid}/recommended", :required => [:user_nid], :optional => [], :example => "/users/", :description => {  } }
       end
       def tnew
-        { :endpoint => "/users/{:nid}/thumbs/new", :required => [:nid,:user_nid,:value,:token], :optional => [], :example => "/users/", :description => {  } }
+        { :endpoint => "/users/{:user_nid}/thumbs/new", :required => [:user_nid,:value,:auth_token], :optional => [:location_nid,:to_user_nid], :example => "/users/", :description => {  } }
       end
       def uthumbs
-        { :endpoint => "/users/{:nid}/thumbs", :required => [:nid], :optional => [], :example => "/users/", :description => {  } }
+        { :endpoint => "/users/{:user_nid}/thumbs", :required => [:user_nid], :optional => [], :example => "/users/", :description => {  } }
       end
       def uthumbed
-        { :endpoint => "/users/{:nid}/thumbed", :required => [:nid], :optional => [], :example => "/users/", :description => {  } }
+        { :endpoint => "/users/{:user_nid}/thumbed", :required => [:user_nid], :optional => [], :example => "/users/", :description => {  } }
       end
       
       #locations
       def lnew
-        { :endpoint => "/locations/new", :required => [:nid,:token,:lat,:lng,:name,:primary], :optional => [:text,:street,:city,:secondary], :example => "/locations/new", :description => {  } }
+        { :endpoint => "/locations/new", :required => [:user_nid,:auth_token,:lat,:lng,:name,:primary], :optional => [:text,:street,:city,:secondary], :example => "/locations/new", :description => {  } }
       end
       def lsearch
-        { :endpoint => "/locations/search", :required => [:q], :optional => [:nid,:lat,:lng,:street,:city], :example => "/locations/search", :description => {  } }
+        { :endpoint => "/locations/search", :required => [:q], :optional => [:location_nid,:lat,:lng,:street,:city], :example => "/locations/search", :description => {  } }
       end
       def here
         { :endpoint => "/locations/here", :required => [:lat,:lng], :optional => [:dist,:primary,:secondary,:cost], :example => "/locations/here?lat=37.337&lng=-122.247&dist=0.625", :description => {  } }
       end
       def ldetail
-        { :endpoint => "/locations/detail", :required => [:nid], :optional => [:nids], :example => "/locations/detail?nids={comma,separated,nids}", :description => {  } }
+        { :endpoint => "/locations/detail", :required => [:location_nid], :optional => [:location_nids], :example => "/locations/detail?location_nids={comma,separated,location_nids}", :description => {  } }
       end
       def rrecommendatons
-        { :endpoint => "/locations/{:nid}/recommendations", :required => [:nid], :optional => [], :example => "/locations/{:nid}/recommendations", :description => {  } }
+        { :endpoint => "/locations/{:location_nid}/recommendations", :required => [:location_nid], :optional => [], :example => "/locations/{:location_nid}/recommendations", :description => {  } }
       end
       def tnew
-        { :endpoint => "/locations/{:nid}/thumbs/new", :required => [:nid,:user_nid,:value,:token], :optional => [], :example => "/locations/", :description => {  } }
+        { :endpoint => "/locations/{:location_nid}/thumbs/new", :required => [:location_nid,:user_nid,:value,:auth_token], :optional => [], :example => "/locations/", :description => {  } }
       end
       def rthumbs
-        { :endpoint => "/locations/{:nid}/thumbs", :required => [:nid], :optional => [], :example => "/locations/", :description => {  } }
+        { :endpoint => "/locations/{:location_nid}/thumbs", :required => [:location_nid], :optional => [], :example => "/locations/", :description => {  } }
       end
       
       # recommendations
--- app/helpers/status.rb
+++ app/helpers/status.rb
@@ -1,6 +1,10 @@
 module Status
   class << self
     
+    def method_missing(method_name, *args)
+      message(-1, "An Error has occurred while calling #{method_name}")
+    end
+    
     def message(status, message, options={})
       message = options[:message] || message
       {:status => status, :message => message, :results => []}
@@ -15,11 +19,7 @@ module Status
     end
     
     def user_not_authorized
-      message(-1, "you are not authorized to view that user")
-    end
-    
-    def image_not_saved
-      message(-1, "the image was not able to be saved.")
+      message(-1, "you are not authorized as that user")
     end
     
     def image_saved(image_nid)
@@ -34,118 +34,43 @@ module Status
     def search_result(results)
       status_message_results(1,'OK',results)
     end
-    
-    def not_found
-      message(-1,'nothing was found that matched the query')
-    end
-    
-    def thumb_created
-      message(1, "the thumb was created succussfully")
-    end
-    
-    def couldnt_create_new_thumb
-      message(-1, "the thumb you tried to create failed, make sure to include the :value param")
-    end
-    
-    def couldnt_follow_or_unfollow
-      message(-1, "The specificed user couldn't be { followed, unfollowed }")
-    end
-    
-    def no_followers
-      message(-1, "it appears that user dont { have any followers, follow anyone }")
-    end
-    
+
     def follow_list(list,options={})
       status_message_results(1,'OK',list,options)
     end
-    
-    def comments_not_found
-      message(-1, 'no comments were found for the specified item')
-    end
-    
-    def recommendation_not(options={})
-      word = options[:word] || "made"
-      message(-1, "The recommendation was not #{word}")
-    end
-    
-    def couldnt_complete_recommendation(action)
-      message(-1, "Your request to #{action} the recommendation couldn't be completed :/")
-    end
-    
-    def no_recommendations(options={})
-      msg = options[:message] || "There were no recommendations for that #{options[:empty] || '{ user, location }'}"
-      message(-1, msg)
-    end
-    
-    def no_ranks_for(options={})
-      msg = options[:message] || "There are no rankings for that #{options[:what]} yet :("
-      message(-1, msg)
-    end
-    
-    def rank_action(opt={})
-      msg opt[:which] || 'done something with'
-      message(1,"rank successfully #{msg}")
-    end
-    
+
     def insufficient_arguments(options={})
       msg = "insufficient or malformed argument #{options[:which]}" if options[:which]
       msg ||= options[:message] || "insufficient or malformed argument(s)"
       message(-1, msg)
     end
-    
-    def no_locations_found
-      message(-1, "There were no locations found")
-    end
-
-    def locations(locs)
-      status_message_results(1, 'OK',locs)
-    end
 
     def user_auth_invalid
-      message(-1, "authentication failed")
+      message(-1, "User authentication via auth_token failed")
+    end
+
+    def not_found(what_is_it='items')
+      message(-1,"no #{what_is_it} were found.")
+    end
+
+    def item_not_destroyed(what='item')
+      message(-1,"The #{what} could not be destroyed")
     end
     
-    def item_not_created
-      message(-1,"the item could not be created")
+    def item_destroyed(what='item')
+      message(1,"The #{what} was destroyed successfully")
     end
     
-    def item_created
-      message(1,"the item was created successfully")
+    def item_not_created(what='item')
+      message(-1,"The #{what} could not be created")
+    end
+    
+    def item_created(what='item')
+      message(1,"The #{what} was created successfully")
     end
     
     def unknown_error
-      message(-1, "an unknown error has occurred")
-    end
-    
-    def thumbs(thmbs)
-      status_message_results(1,"fetched thumbs successfully",thumbs, {:result_name => :thumbs})
-    end
-    
-    def detail
-      
-    end
-    
-    def detail_not_found(detail)
-      status_message_results(1,'OK', detail, :detail)
-    end
-    
-    def user_detail(user)
-      unless this = user.first
-        message(-1,"no user found for the id")
-      else
-        status_message_results this.hashify?, 1, "OK"
-      end
-    end
-    
-    def user_session(user, session)
-      unless user[0]
-        message(-1,"no user found for the id")
-      else
-        # User.hashify!(user)
-        response = status_message_results user, 1, "OK"
-        response[:session] = session
-        response
-      end
+      message(-1, "An API error has occurred")
     end
 
     def location_ranks(location,ranks)
@@ -153,14 +78,6 @@ module Status
         :status => 1,
         :message => 'OK',
         :location => location,
-        :ranks => ranks
-      }
-    end
-
-    def ranks(ranks)
-      {
-        :status => 1,
-        :message => 'OK',
         :ranks => ranks
       }
     end
@@ -177,16 +94,12 @@ module Status
     private
     
     def status_message_results(status,message,results,options={})
-      if results.nil?
-        result_name = :messags
-        results = ['everything went fine on our end']
-      else
-        result_name = options[:result_name] || :results
-      end
+      results ||= []
+      result_name = options[:result_name] || :results
       {
         :status      => status,
         :message     => message,
-         result_name => results
+         result_name => Array(results)
       }
     end
   end
