38606bc2314c9a7a103dd7216921cd94c57a9688
Brian Norton <brian.nort@gmail.com>
2014-10-02 06:57:52 -0700

    Delete secret_token.rb

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-NomIt::Application.config.secret_token = 'cfa886982b485501063fbc0a5accc3e53dcd8cf75590f3e18600e82a8c29e3bcdeef83d0bf8ae63202bd31ebe0a2cf5346148018191ef2f91a1c2c75588f5be5'
